bf5e37015bec842a2222e8446d209a265ba03b55
Zeno Jiricek <airtonix@users.noreply.github.com>
2025-12-14 18:03:40 +1030

    [refactor] modernize mod organization and fix formatting issues

--- pkgs/Zeus/config.cpp
+++ pkgs/Zeus/config.cpp
@@ -1,1 +1,34 @@
-#include "Zeus/Scripts/config.cpp"
+// Zeus Framework Configuration
+// This mod provides advanced scripting and administrative tools for DayZ servers
+
+class CfgPatches
+{
+	class Zeus
+	{
+		requiredAddons[] =
+		{
+			// 
+		};
+	};
+};
+
+class CfgMods
+{
+    class Zeus
+    {
+		type = "mod";
+
+        class defs
+		{
+			
+			class worldScriptModule
+			{
+				value = "";
+				files[] =
+				{
+					"Scripts/4_World"
+				};
+			};
+		};
+    };
+};
