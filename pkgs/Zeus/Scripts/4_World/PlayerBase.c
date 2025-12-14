0ce6217f4a1d2d2cb0c20595567fa41b651f279b
Zeno Jiricek <airtonix@users.noreply.github.com>
2025-12-14 18:50:38 +1030

    remove template mod

--- pkgs/Zeus/Scripts/4_World/PlayerBase.c
+++
@@ -1,8 +0,0 @@
-modded class PlayerBase	// modded keyword for modding an existing class
-{
-	override void OnJumpStart()	// overriding an existing method
-	{
-		super.OnJumpStart();			// call the original jump callback method so we don't break stuff
-		Print("My first mod, yay!");	// our modded print
-	}
-}
